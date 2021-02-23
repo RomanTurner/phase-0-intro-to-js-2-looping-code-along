f15dbc0be936db1bb58035dfbf6444e3de585be4
Roman Turner <rn.turner@live.com>
2021-02-23 15:29:26 -0800

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function countDown(countdown) {
+  while (countdown >= 0) {
+    console.log(countdown--);
+  }
+};
+
+
+
+function writeCards(guest, event) {
+    const newArr = [];
+    for (let i = 0; i < guest.length; i++){
+       newArr.push(`Thank you, ${guest[i]}, for the wonderful ${event} gift!`)
+    }
+    return newArr;
+};
+
+
